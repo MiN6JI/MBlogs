47dd9fb0300eeed33c12c0cfd424580fe817f279
MINGJI <deorukhkarshruti232@gmail.com>
2025-06-02 15:47:06 +0530

    Changes

---
+++ pages/Auth/Login.vue
@@ -0,0 +1,6 @@
+<template>
+  <UContainer class="w-full flex flex-row">
+    <div class="w-1/2">Use Auth Layouts (First Create a Layout)</div>
+    <div class="w-1/2">hi</div>
+  </UContainer>
+</template>
