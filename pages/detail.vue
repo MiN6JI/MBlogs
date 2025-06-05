654bca24553a071c886b449fd9e2893e3e59be9a
MINGJI <deorukhkarshruti232@gmail.com>
2025-06-05 17:16:24 +0530

    Changes

---
+++ pages/detail.vue
@@ -0,0 +1,32 @@
+<template>
+  <UContainer class="p-10">
+    <div class="p-4 w-full rounded-xl bg-gray-100">
+      <div class="w-[60%]">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel voluptas
+        numquam similique at. Tempora temporibus quibusdam fugiat molestias?
+        Maiores, iure eius molestias at, eveniet optio fugit minima dolore
+        labore aliquid doloremque magni quidem sapiente eum unde est!
+        Accusantium, odit. Veritatis, unde optio sint, quod sit alias
+        consequuntur architecto voluptatem necessitatibus mollitia temporibus
+        molestiae veniam ullam recusandae esse, doloremque saepe perferendis.
+        Ab, voluptate obcaecati fugiat alias earum nulla nostrum provident
+        adipisci non. Doloribus repudiandae atque possimus fugit culpa
+        recusandae ducimus harum corporis ab, nihil nobis, exercitationem
+        debitis consectetur aliquid. Officia sit quaerat sint aliquid modi,
+        recusandae, natus expedita illum architecto commodi fuga exercitationem
+        perspiciatis molestias adipisci voluptatum aliquam dolor quae? Ut,
+        inventore. Minima omnis error rerum, consequatur commodi distinctio!
+        Corrupti voluptate facilis qui quia sit et quibusdam est ullam omnis
+        dolores eius molestias iusto explicabo quaerat quam ipsum tempora modi,
+        cumque voluptatibus voluptates, sapiente voluptas consequuntur illo
+        ratione! Accusamus veniam omnis minus quia nisi sint doloremque
+        cupiditate inventore tempore nesciunt minima nostrum excepturi
+        accusantium, enim quae, ut nemo, et eos. Maiores iste porro nihil culpa
+        sequi omnis natus repellat quis, voluptate praesentium voluptates,
+        aspernatur aut? Voluptatem mollitia deserunt, nobis cupiditate dolor
+        laboriosam, sed ex adipisci blanditiis quas minus nemo veritatis
+        sapiente.
+      </div>
+    </div>
+  </UContainer>
+</template>
--- pages/posts.vue/detail.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    This is a Blog Detail Page
-</template>
