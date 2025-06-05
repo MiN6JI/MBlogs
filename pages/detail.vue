3265ae1242d30d15dc4526bc6c8f13ca26b8f326
MINGJI <deorukhkarshruti232@gmail.com>
2025-06-05 17:36:40 +0530

    Chanegs

--- pages/detail.vue
+++ pages/detail.vue
@@ -1,32 +1,72 @@
 <template>
   <UContainer class="p-10">
-    <div class="p-4 w-full rounded-xl bg-gray-100">
+    <div class="p-10 w-full flex flex-col items-center rounded-xl bg-gray-100">
       <div class="w-[60%]">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel voluptas
-        numquam similique at. Tempora temporibus quibusdam fugiat molestias?
-        Maiores, iure eius molestias at, eveniet optio fugit minima dolore
-        labore aliquid doloremque magni quidem sapiente eum unde est!
-        Accusantium, odit. Veritatis, unde optio sint, quod sit alias
-        consequuntur architecto voluptatem necessitatibus mollitia temporibus
-        molestiae veniam ullam recusandae esse, doloremque saepe perferendis.
-        Ab, voluptate obcaecati fugiat alias earum nulla nostrum provident
-        adipisci non. Doloribus repudiandae atque possimus fugit culpa
-        recusandae ducimus harum corporis ab, nihil nobis, exercitationem
-        debitis consectetur aliquid. Officia sit quaerat sint aliquid modi,
-        recusandae, natus expedita illum architecto commodi fuga exercitationem
-        perspiciatis molestias adipisci voluptatum aliquam dolor quae? Ut,
-        inventore. Minima omnis error rerum, consequatur commodi distinctio!
-        Corrupti voluptate facilis qui quia sit et quibusdam est ullam omnis
-        dolores eius molestias iusto explicabo quaerat quam ipsum tempora modi,
-        cumque voluptatibus voluptates, sapiente voluptas consequuntur illo
-        ratione! Accusamus veniam omnis minus quia nisi sint doloremque
-        cupiditate inventore tempore nesciunt minima nostrum excepturi
-        accusantium, enim quae, ut nemo, et eos. Maiores iste porro nihil culpa
-        sequi omnis natus repellat quis, voluptate praesentium voluptates,
-        aspernatur aut? Voluptatem mollitia deserunt, nobis cupiditate dolor
-        laboriosam, sed ex adipisci blanditiis quas minus nemo veritatis
-        sapiente.
+        <div>
+          <div class="w-full flex flex-row py-6">
+            <div class="w-1/2">
+              <div class="text-gray normal-text">October 5, 2025</div>
+            </div>
+            <div class="w-1/2 flex justify-end">
+              <div
+                class="inline text-xs font-medium py-1 px-4 rounded-2xl bg-[var(--color-mint)]"
+              >
+                Blog Tags
+              </div>
+            </div>
+          </div>
+
+          <div>
+            <img
+              src="https://picsum.photos/1920/768.webp"
+              alt="blog posts"
+              class="rounded-xl"
+            />
+          </div>
+          <div class="py-2">
+            <div class="sub-heading">
+              Financial Planning for Entrepreneurs: A Success Blueprint
+            </div>
+          </div>
+        </div>
+        <p class="normal-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis
+          dolore non molestiae? Fugiat quia mollitia ipsam, beatae laudantium
+          deleniti tempore assumenda corporis aperiam blanditiis facilis
+          dolorem, maiores consectetur rerum at aut est eveniet ab iste
+          cupiditate delectus! Perspiciatis explicabo repudiandae at officia
+          cumque voluptatem earum tenetur ab accusamus voluptatibus.
+        </p>
+        <p class="normal-text pt-4">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda,
+          quis dolores quibusdam dicta deserunt architecto explicabo beatae
+          voluptates dolor aut id ipsa nihil magni porro molestiae ex numquam
+          hic modi quasi iste vel? Eligendi, eum iusto veniam dolorem vitae
+          nesciunt voluptates expedita alias in deserunt. Magnam ex tempora
+          explicabo sint. Quod at quasi unde expedita totam reiciendis deleniti
+          possimus tempore enim natus facilis pariatur iure dolore fugit vel
+          culpa magni beatae nulla tempora, minima quis? Velit, dignissimos a,
+          animi pariatur quaerat autem reiciendis ad aut, praesentium suscipit
+          placeat quas dolores accusantium? Cupiditate pariatur laborum illo qui
+          aliquid corporis repudiandae nisi?
+        </p>
+        <p class="pt-4 normal-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa omnis
+          dolore non molestiae? Fugiat quia mollitia ipsam, beatae laudantium
+          deleniti tempore assumenda corporis aperiam blanditiis facilis
+          dolorem, maiores consectetur rerum at aut est eveniet ab iste
+          cupiditate delectus! Perspiciatis explicabo repudiandae at officia
+          cumque voluptatem earum tenetur ab accusamus voluptatibus.
+        </p>
       </div>
+    </div>
+  </UContainer>
+  <UContainer>
+    <div class="sub-heading text-center">Your Posts</div>
+    <div class="flex-justify pt-10 gap-6">
+      <div class="w-1/3"><SinglePost /></div>
+      <div class="w-1/3"><SinglePost /></div>
+      <div class="w-1/3"><SinglePost /></div>
     </div>
   </UContainer>
 </template>
