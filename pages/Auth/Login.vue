f4077143bdcd983e7e7a15e08e6afcaabe783d86
MINGJI <deorukhkarshruti232@gmail.com>
2025-06-25 23:13:43 +0530

    Chanegs

--- pages/Auth/Login.vue
+++ pages/Auth/Login.vue
@@ -10,44 +10,46 @@
           Lorem ipsum dolor sit amet consectetur, adipisicing elit Autem.
         </p>
       </div>
-      <div class="flex flex-col gap-4 py-6">
-        <UInput
-          class="w-full"
-          v-model="email"
-          type="email"
-          placeholder="Email"
-          size="xl"
-          :ui="{ base: 'w-full rounded-3xl py-3 px-6' }"
-        />
-        <UInput
-          v-model="password"
-          placeholder="Password"
-          :type="show ? 'text' : 'password'"
-          :ui="{ trailing: 'pe-1', base: 'w-full rounded-3xl py-3 px-6' }"
-          size="xl"
-        >
-          <template #trailing>
-            <UButton
-              color="neutral"
-              variant="link"
-              size="md"
-              :icon="show ? 'i-lucide-eye' : 'i-lucide-eye-off'"
-              :aria-pressed="show"
-              aria-controls="password"
-              :ui="{ base: 'pr-6' }"
-              @click="show = !show"
-            />
-          </template>
-        </UInput>
-        <UButton
-          size="xl"
-          class="mt-4 text-center bg-[var(--color-jet)]"
-          type="submit"
-          label="Submit"
-          to="/auth/register"
-          :ui="{ base: 'w-full rounded-3xl py-3 px-6' }"
-        />
-      </div>
+      <UForm :state="formInputs" @submit.prevent="submit">
+        <div class="flex flex-col gap-4 py-6">
+          <UInput
+            class="w-full"
+            v-model="formInputs.email"
+            type="email"
+            placeholder="Email"
+            size="xl"
+            :ui="{ base: 'w-full rounded-3xl py-3 px-6' }"
+          />
+          <UInput
+            v-model="formInputs.password"
+            placeholder="Password"
+            :type="show ? 'text' : 'password'"
+            :ui="{ trailing: 'pe-1', base: 'w-full rounded-3xl py-3 px-6' }"
+            size="xl"
+          >
+            <template #trailing>
+              <UButton
+                color="neutral"
+                variant="link"
+                size="md"
+                :icon="show ? 'i-lucide-eye' : 'i-lucide-eye-off'"
+                :aria-pressed="show"
+                aria-controls="password"
+                :ui="{ base: 'pr-6' }"
+                @click="show = !show"
+              />
+            </template>
+          </UInput>
+          <UButton
+            size="xl"
+            class="mt-4 text-center bg-[var(--color-jet)]"
+            type="submit"
+            label="Submit"
+            :ui="{ base: 'w-full rounded-3xl py-3 px-6' }"
+          />
+          <!-- to="/auth/register" -->
+        </div>
+      </UForm>
     </div>
   </div>
 </template>
@@ -58,23 +60,27 @@ definePageMeta({
   layout: "auth",
 });
 
-// const formInputs = ref({
-//   name: "",
-//   email: "",
-//   password: null,
-// });
+const formInputs = reactive({
+  email: "",
+  password: null,
+});
 
 const email = ref("");
 const show = ref(false);
 const password = ref("");
 
 async function submit() {
-  await $apiFetch("/login", {
-    method: "POST",
-    body: {
-      email: email.value,
-      password: password.value,
-    },
-  });
+  const formData = new FormData();
+
+  formData.append("email", formInputs.email);
+  formData.append("password", formInputs.password);
+  try {
+    const response = await useNuxtApp().$apiFetch("/login", {
+      method: "POST",
+      body: formData,
+    });
+  } catch (error) {
+    console.error("Submission failed:", error);
+  }
 }
 </script>
