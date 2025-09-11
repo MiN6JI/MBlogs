76bb2a3da6c1ef4cd0d1b3825a67dcd981dee7c2
MINGJI <deorukhkarshruti232@gmail.com>
2025-09-11 13:53:05 +0530

    Changes

--- plugins/apiFetch.js
+++ plugins/apiFetch.js
@@ -1,27 +1,26 @@
 import { defineNuxtPlugin } from "#app";
 import Cookies from "js-cookie";
 
-export default defineNuxtPlugin(nuxtApp => {
-   const config = useRuntimeConfig()
+export default defineNuxtPlugin((nuxtApp) => {
+  const config = useRuntimeConfig();
 
-   const apiFetch = $fetch.create({
-      baseURL: config.public.apiBase,
-      credentials: 'include',
-      headers: {
-         Accept: 'application/json',
-         // 'X-XSRF-TOKEN': Cookies.get('XSRF-TOKEN'),
-         onRequest({ options }) {
-            // Dynamically read the cookie on every request
-            const token = Cookies.get('XSRF-TOKEN')
-            if (token) {
-               options.headers = {
-                  ...options.headers,
-                  'X-XSRF-TOKEN': token,
-               }
-            }
-         },
+  const apiFetch = $fetch.create({
+    baseURL: config.public.apiBase,
+    credentials: "include",
+    headers: {
+      Accept: "application/json",
+    },
+    onRequest({ options }) {
+      // Dynamically read the cookie on every request
+      const token = Cookies.get("XSRF-TOKEN");
+      if (token) {
+        options.headers = {
+          ...options.headers,
+          "X-XSRF-TOKEN": token,
+        };
       }
-   });
+    },
+  });
 
-   nuxtApp.provide('apiFetch', apiFetch);
+  nuxtApp.provide("apiFetch", apiFetch);
 });
